b3fb28fb17c52d98eb6eb8d6daeabb5febaa1910
goong <wnsgur2179@naver.com>
2023-12-11 20:37:32 +0900

    gpio pin 수정

--- InterfaceRaspi/Raspi_Itr.h
+++ InterfaceRaspi/Raspi_Itr.h
@@ -2,10 +2,10 @@
     #define RASPI_ITR_H
 
 /* Port Setup */
-#define PROT_INT_A1 18   //GPIO18 PWM0 Pin12
-#define PROT_INT_A2 17   //GPIO17      Pin11
-#define PROT_INT_B1 13   //GPIO18 PWM1 Pin12
-#define PROT_INT_B2 19   //GPIO17      Pin11
+#define PROT_INT_A1 23   //GPIO23      Pin12
+#define PROT_INT_A2 24   //GPIO24      Pin11
+#define PROT_INT_B1 20   //GPIO20      Pin12
+#define PROT_INT_B2 21   //GPIO21      Pin11
 
 
 #endif // #define RASPI_ITR_H
