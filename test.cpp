eed9eafc94f047e69d290c351ed15fc92d96d46d
goongg <wnsgur2179@naver.com>
2023-12-16 05:16:35 +0900

    testcode

--- test.cpp
+++ test.cpp
@@ -1,182 +1,71 @@
-//#include <iostream>
-//#include <fcntl.h>
-//#include <unistd.h>
-//#include <linux/joystick.h>
-//class GamePad  {
-//public:
-//    GamePad();
-//    ~GamePad();
-//    unsigned short getButtonState() ;
-//    void getJoystickState(int* posX, int* posY) ;
-//    int getTriggerState(bool isRightTrigger) ;
-
-//private:
-//};
-
-//GamePad::GamePad() {
-//    int fd;
-//    fd = open("/dev/input/js0", O_RDONLY);
-//    if (fd == -1) {
-//        std::cerr << "Unable to open joystick device." << std::endl;
-//        close(fd);        
-//        return;
-//    }
-//    close(fd);
-//}
-
-//GamePad::~GamePad() {
-//}
-
-//unsigned short GamePad::getButtonState() {
-//    js_event js;
-//    ssize_t bytesRead;
-//    int fd;
-//    fd = open("/dev/input/js0", O_RDONLY);
-//    bytesRead = read(fd, &js, sizeof(js_event));
-//    if (bytesRead == -1) {
-//        std::cerr << "Error reading joystick input." << std::endl;
-//         close(fd);        
-//        return 1;
-//    }
-
-//    // Check if it's an event for button press or release
-//    if ((js.type & JS_EVENT_BUTTON) && js.value == 1) {
-//        return (1 << js.number); // Button index is zero-based
-//    }
-//    close(fd);
-//    return 0;
-//}
-
-//void GamePad::getJoystickState(int* posX, int* posY) {
-//    js_event js;
-//    ssize_t bytesRead;
-//    int fd;
-//    fd = open("/dev/input/js0", O_RDONLY);
-//    bytesRead = read(fd, &js, sizeof(js_event));
-//    if (bytesRead == -1) {
-//        close(fd);        
-//    }
-
-//    // Check if it's an event for joystick movement
-//    if (js.type & JS_EVENT_AXIS) {
-//        if (js.number == 0) {
-//            *posX = js.value;
-//        } else if (js.number == 1) {
-//            *posY = js.value;
-//        }
-//    }
-//}
-
-//int GamePad::getTriggerState(bool isRightTrigger) {
-//    js_event js;
-//    ssize_t bytesRead;
-//    int fd;
-//    fd = open("/dev/input/js0", O_RDONLY);    
-//    bytesRead = read(fd, &js, sizeof(js_event));
-//    if (bytesRead == -1) {
-//        close(fd);        
-//    }
-
-//    // Check if it's an event for trigger movement
-//    if (js.type & JS_EVENT_AXIS) {
-//        if (isRightTrigger && js.number == 4) {
-//            return js.value;
-//        } else if (!isRightTrigger && js.number == 5) {
-//            return js.value;
-//        }
-//    }
-//        close(fd);
-//    return 0;
-//}
-
-
-
-
-
+#include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <sys/ioctl.h>
-#include </usr/include/linux/joystick.h>
+#include <linux/joystick.h>
 
- 
+#define JOY_DEV "/dev/input/js0"
 
-int joy_fd, *axis=NULL, num_of_axis=0, num_of_buttons=0, x;
-char *button=NULL, name_of_joystick[80];
+int main()
+{
+	int joy_fd, *axis=NULL, num_of_axis=0, num_of_buttons=0, x;
+	char *button=NULL, name_of_joystick[80];
+	struct js_event js;
 
-struct js_event js;
-int Left_Forward;
-int Left_Reverse;
-int Right_Forward;
-int Right_Reverse;
- 
-
-void Joystick(void)
-{   
-     read(joy_fd, &js, sizeof(struct js_event));       // 조이스틱 상태를 읽어오는 부분
-     switch (js.type & ~JS_EVENT_INIT)
-     {
-             case JS_EVENT_AXIS:
-                     axis   [ js.number ] = js.value;
-                     break;
-             case JS_EVENT_BUTTON:
-                     button [ js.number ] = js.value;
-                     break;
-             case JS_EVENT_INIT:
-                     button [ js.number ] = js.value;
-                     break;           
-     }
-
-     Left_Forward = axis[0];
-     Left_Reverse = axis[1];
-    
-     if( num_of_axis > 2 )   
-     Right_Forward = axis[2];
-     if( num_of_axis > 3 )
-     Right_Reverse = axis[3];
-    
-     printf("X: %6d  ", axis[0] );
-     printf("Y: %6d  ", axis[1] );
- 
-     for( x=0 ; x<num_of_buttons ; ++x )
-     for( x=0 ; x<8 ; ++x )
-             printf("B%d: %d  ", x, button[x] );
-     printf("\r");
-     printf("\n");
-     fflush(stdout);
-}
-
-int main() {
-    //GamePad gamePad;
-
-    // Test the controller functions
-    int posX = 0, posY = 0;
-    unsigned short buttonState = 0;
-    int rightTriggerState = 0, leftTriggerState = 0;
-//	std::cout<<"?";
-	if(joy_fd= open("/dev/input/js0", O_RDONLY) ==-1)
+	if( ( joy_fd = open( JOY_DEV , O_RDONLY)) == -1 )
 	{
-		printf("error");
+		printf( "Couldn't open joystick\n" );
 		return -1;
 	}
-	
-	ioctl(joy_fd, JSIOCGAXES, &num_of_axis);	
-	ioctl(joy_fd, JSIOCGBUTTONS, &num_of_buttons);	
-//	ioctl(joy_fd, JSIOCGNAME, &name_of_joystick);	
-		 
-	axis = (int *) calloc(num_of_axis, sizeof(int));	
-	button = (char *) calloc(num_of_buttons, sizeof(char));
-	
-	fcntl(joy_fd, F_SETFL, O_NONBLOCK);
-	
-    // Read joystick input in a loop (you may want to use threading to handle input asynchronously)
-    while (true) {
-//		std::cout<<"?";
-		
-       Joystick();
-       // usleep(10000); // Sleep for 10 milliseconds to avoid busy-waiting
-    }
 
-    return 0;
+	ioctl( joy_fd, JSIOCGAXES, &num_of_axis );
+	ioctl( joy_fd, JSIOCGBUTTONS, &num_of_buttons );
+	ioctl( joy_fd, JSIOCGNAME(80), &name_of_joystick );
+
+	axis = (int *) calloc( num_of_axis, sizeof( int ) );
+	button = (char *) calloc( num_of_buttons, sizeof( char ) );
+
+	printf("Joystick detected: %s\n\t%d axis\n\t%d buttons\n\n"
+		, name_of_joystick
+		, num_of_axis
+		, num_of_buttons );
+
+	fcntl( joy_fd, F_SETFL, O_NONBLOCK );	/* use non-blocking mode */
+
+	while( 1 ) 	/* infinite loop */
+	{
+
+			/* read the joystick state */
+		read(joy_fd, &js, sizeof(struct js_event));
+		
+			/* see what to do with the event */
+		switch (js.type & ~JS_EVENT_INIT)
+		{
+			case JS_EVENT_AXIS:
+				axis   [ js.number ] = js.value;
+				break;
+			case JS_EVENT_BUTTON:
+				button [ js.number ] = js.value;
+				break;
+		}
+
+			/* print the results */
+		printf( "X: %6d  Y: %6d  ", axis[0], axis[1] );
+		
+		if( num_of_axis > 2 )
+			printf("Z: %6d  ", axis[2] );
+			
+		if( num_of_axis > 3 )
+			printf("R: %6d  ", axis[3] );
+			
+		for( x=0 ; x<num_of_buttons ; ++x )
+			printf("B%d: %d  ", x, button[x] );
+
+		printf("  \r");
+		fflush(stdout);
+	}
+
+	close( joy_fd );	/* too bad we never get here */
+	return 0;
 }
